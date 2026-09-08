01d6055e1aa8c85e355d3753cd3c5263106a439b
hli852-vtc-student <145430301+hli852-vtc-student@users.noreply.github.com>
2026-09-08 02:03:13 +0000

    [skip actions] lab/lab01/ch01_t02_print_statements.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-API_KEY = ""
-API_ENDPOINT = ''
+API_KEY = "AIzaSyCO0Bw8U7_uo7ahJo6XleD_2QMcnuU9x0k"
+API_ENDPOINT = 'https://gateway-7cyi7dui.ue.gateway.dev'
--- lab/lab01/ch01_t01_hello_world.py
+++ lab/lab01/ch01_t01_hello_world.py
@@ -1,1 +1,1 @@
-print("hello worygyugugld")
+print("Hello, world!")
